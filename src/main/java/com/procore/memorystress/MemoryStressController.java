872f1e5b398157dc0e8efdbf6afbbfea81d21e86
Volodymyr Sobotovych <volodymyr.sobotovych-contractor@procore.com>
2022-10-13 19:16:59 +0300

    Initial commit

---
+++ src/main/java/com/procore/memorystress/MemoryStressController.java
@@ -0,0 +1,25 @@
+package com.procore.memorystress;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+public class MemoryStressController {
+
+    private final List<Byte> byteBuffer = new LinkedList<>();
+
+    @PostMapping("/fill")
+    public String fillMemory(@RequestParam("size") long size) {
+        byteBuffer.clear();
+
+        for (int i = 0; i < size; i++) {
+            byteBuffer.add((byte) (i % 100));
+        }
+
+        return "Done. Size: " + byteBuffer.size() + "\n";
+    }
+}
---
+++ src/test/java/com/procore/memorystress/MemoryStressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procore.memorystress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryStressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
